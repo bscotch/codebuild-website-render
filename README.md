4c383e0a50d117c055c13ddbae60654fb517b65c
Adam Coster <adam@bscotch.net>
2020-09-27 15:32:25 -0500

    feat: Draft full functionality, including ability to specify sitemap target via environment variables and render all pages.

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ since build-time pre-rendering is not always applicable in those cases.
 
 ## Usage
 
+### Create a CodeBuild Project
+
+1. Go to the [CodeBuild console](https://console.aws.amazon.com/codesuite/codebuild/projects.)
+2. [Create a new project](https://console.aws.amazon.com/codesuite/codebuild/project/new).
+  + **Project Configuration**
+    + Name it something descriptive.
+  + **Source**
+    + Consider making a fork of this project and pointing to that.
+    + If you are going to use this repo as the source, specify the hash so that it doesn't auto-update on you!
+  + **Environment**
+    + Select Ubuntu (Amazon Linux might also work, but it's untested)
+    + Select the latest runtimes etc.
+    + Create or choose a Service Role. You'll need to ensure it has the proper IAM permissions.
+  + **Buildspec**
+    + Choose "Use a Buildspec file"
+  + **Artifacts**
+    + Choose "Amazon S3"
+    + Choose your bucket (you may want to make a new one)
+    + Set any other options that make sense for your deployment. Include a prefix if the S3 bucket is used for other things.
+    + Consider adding S3 caching. This project depends on downloading a large (250+MB) Chrome executable. Not having to do that every time will probably speed things up!
+3. Set the source to this project
+  + It's a good idea to make a fork and point to that, so that when this project changes yours won't automatically change too!
+  + Alternatively, specify the commit hash so that it won't auto-update on future builds.
+4. Choose
--- index.js
+++ index.js
@@ -1,40 +1,127 @@
+/**
+ * @file Main for rendering pages of a website as HTML via
+ * either sitemaps or lists of URLs. Arguments come in via
+ * environment variables, either set up in the CodeBuild
+ * project or passed in via an AWS CodeBuild API call via the
+ * environmentVariablesOverride parameter.
+ * 
+ * ## PARAMETERS (via env)
+ * 
+ *  + BASE_URL (e.g. "https://www.bscotch.net")
+ *  + SITEMAP_PATH (e.g. "/sitemap.xml")
+ *    Either this or paths must be specified.
+ *    Must be a path relative to BASE_URL
+ *  + PATHS (CSV, e.g. "/,/about,/blog/post-1")
+ *    Must be relative to BASE_URL
+ *  + OUT_FOLDER (e.g. "dev")
+ *    Target 'folder' (key prefix) within the S3 bucket + prefix
+ *    defined in the CodeBuild project.
+ */
+
+
 const pptr = require('puppeteer');
+const Sitemapper = require('sitemapper');
+const sitemapper = new Sitemapper();
+const fs = require('fs');
+const path = require('path');
+const events = require('events');
+
+const outDir = 'rendered';
+
+const emitter = new events.EventEmitter();
+
+/**
+ * @param {Parameters} params
+ * @param {string} url 
+ * @param {string} html 
+ */
+function writeRenderedPage(params,url,html){
+  let fullPath = path.join(outDir,params.outFolder,url);
+  if(!fullPath.endsWith('.html')){
+    fullPath = path.join(fullPath,'index.html');
+  }
+  const dir = path.dirname(fullPath);
+  fs.mkdirSync(dir,{recursive:true});
+  fs.writeFileSync(fullPath,html);
+  emitter.emit('write',{url});
+}
+
+/**
+ * @typedef {ReturnType<getParameters>} Parameters
+ */
+
+function assert(somethingTruthy,messageIfFalse){
+  if(!somethingTruthy){
+    throw new Error(messageIfFalse);
+  }
+}
+
+function ensureSlashPrefix(path){
+  return path.startsWith('/') ? path : `/${path}`;
+}
+
+function getParameters(){
+  const baseUrl = process.env.BASE_URL;
+  assert(
+    baseUrl.match(/^https?:\/\/[^/]+$/),
+    "BASE_URL must match the pattern ^https?://[^/]+$"
+  );
+  const sitemapPath = process.env.SITEMAP_PATH
+    ? ensureSlashPrefix(process.env.SITEMAP_PATH)
+    : null;
+  const paths = (process.env.PATHS||'')
+    .split(/\s*,\s*/g)
+    .filter(x=>x)
+    .map(ensureSlashPrefix);
+  assert(sitemapPath || paths.length, "Sitemap path or paths list must be provided.");
+  const outFolder = ensureSlashPrefix(process.env.OUT_FOLDER||'');
+  return {
+    baseUrl,
+    sitemapPath,
+    paths,
+    outFolder
+  }
+}
+
+/**
+ * @param {Parameters} parameters 
+ */
+async function getPaths(parameters){
+  const urls = [...parameters.paths];
+  if(parameters.sitemapPath){
+    // Download it!
+    const {sites} = (await sitemapper.fetch(`${parameters.baseUrl}${parameters.sitemapPath}`));
+    urls.push(...sites);
+  }
+  return urls;
+}
 
 const maxSynchronous = process.env.MAX_SYNCHRONOUS || 20;
 
 /**
  * @param {import('puppeteer').Browser} browser 
  * @param {string} baseUrl
- * @param {string} path
+ * @param {string} url
  */
-async function fetchPage(browser,baseUrl,path){
-  path = path.replace(/^\//,''); // normalize incoming urls
-  const fullUrl = `${baseUrl}/${path}`;
+async function fetchPage(browser,baseUrl,url){
+  url = url.replace(/^\//,''); // normalize incoming urls
+  const fullUrl = `${baseUrl}/${url}`;
   const page = await browser.newPage();
-  page.setExtraHTTPHeaders({
-    'Prerender-Password': process.env.PASSWORD, // for rate limit bypass
-    'Authorization': `Basic ${process.env.DEV_SERVER_BASIC_AUTH}`
-  });
+  // page.setExtraHTTPHeaders({
+  //   'Prerender-Password': process.env.PASSWORD, // for rate limit bypass
+  //   'Authorization': `Basic ${process.env.DEV_SERVER_BASIC_AUTH}`
+  // });
   await page.goto(fullUrl,{waitUntil:'networkidle0'});
   const html = await page.content();
+  console.log('FETCHED',url);
   await page.close();
   return html;
 }
 
-/**
- * @param {string} bucket 
- * @param {string} path 
- * @param {string} html 
- */
-async function uploadPageToS3(bucket,path,html){
-  // TODO: populate this
-  return;
-}
+async function prerenderPaths (){
+  const params = getParameters();
+  const urls = await getPaths(params);
 
-/**
- * @param {{baseUrl:string, bucket:string, paths:string[]}} data
- */
-async function prerenderPaths (data){
   const browser = await pptr.launch();
   // Ensure there are ALWAYS maxSynchronous in queue
   // (each time one ends another one is added).
@@ -43,21 +130,28 @@ async function prerenderPaths (data){
 
   const renderNextUrl = ()=>{
     pathPointer++;
-    return pathPointer >= data.paths.length ||
-      fetchPage(browser,data.baseUrl,data.paths[pathPointer])
-        .then(html=>uploadPageToS3(data.bucket,data.paths[pathPointer],html))
+    if(pathPointer >= urls.length){
+      emitter.emit('done');
+    }
+    const url = urls[pathPointer];
+    return pathPointer >= urls.length ||
+      fetchPage(browser,params.baseUrl,url)
+        .then(html=>writeRenderedPage(params,url,html))
         .then(renderNextUrl); 
   };
 
   for(let i=0; i<maxSynchronous; i++){
     renderNextUrl();
   }
+
+  const rendered = [];
+  emitter.on('write',info=>{
+    rendered.push(info.url);
+    if(rendered.length == urls.length){
+      browser.close();
+    }
+  });
 }
 
-prerenderPaths({
-  baseUrl: process.env.BASE_URL,
-  bucket: process.env.BUCKET,
-  paths: process.env.PRERENDER_PATHS
-    .split(process.env.PRERENDER_PATHS_SEP||';')
-});
+prerenderPaths();
 
