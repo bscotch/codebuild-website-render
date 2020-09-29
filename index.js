c197b40f6e69eeea4ec377f73b3f9a0f50f8c23f
Adam Coster <adam@bscotch.net>
2020-09-29 14:04:17 -0500

    fix: The json extension is missing its dot, causing invalid filenames.

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function writeRenderedPage(params,url,html){
   fs.mkdirSync(dir,{recursive:true});
   fs.writeFileSync(fullPath, params.gzip ? await gzip(html) : html);
   if(params.computeScriptHashes){
-    const metadataPath = fullPath.replace(/\.html(\.gz)?$/,'json');
+    const metadataPath = fullPath.replace(/\.html(\.gz)?$/,'.json');
     if(metadataPath.endsWith('json')){
       const hashes = computeInlineScriptHashes(html);
       const metadataJson = JSON.stringify({scriptHashes:hashes});
